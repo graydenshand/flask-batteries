d7c9b6cd0086ae1d971e66bb1f61ed6d95a1de3f
Grayden <graydenshand@gmail.com>
2021-07-27 15:07:13 -0400

    Deleted old installers dict

--- src/flask_batteries/installers/__init__.py
+++ src/flask_batteries/installers/__init__.py
@@ -3,9 +3,3 @@ from .flask_migrate import FlaskMigrateInstaller
 from .flask_wtf import FlaskWTFInstaller
 from .flask_login import FlaskLoginInstaller
 from .flask_mail import FlaskMailInstaller
-
-installers = {
-    "sqlalchemy": FlaskSQLAlchemyInstaller,
-    "migrate": FlaskMigrateInstaller,
-    "wtf": FlaskWTFInstaller,
-}
